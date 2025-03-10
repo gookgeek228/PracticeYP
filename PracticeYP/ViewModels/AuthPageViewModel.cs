082038a7fbcb37be4fd85fdc5f2e06f700a75b9f
Иван <ivancop89@gmail.com>
2025-03-10 21:35:10 +0300

    Update captcha

--- PracticeYP/ViewModels/AuthPageViewModel.cs
+++ PracticeYP/ViewModels/AuthPageViewModel.cs
@@ -104,7 +104,7 @@ namespace PracticeYP.ViewModels
         private static string GenerateRandomText(int length)
         {
             const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-            return new string(Enumerable.Repeat(chars, length) //Выборка случайных символов в размере 6-ти символов
+            return new string(Enumerable.Repeat(chars, length)
                 .Select(s => s[_random.Next(s.Length)]).ToArray());
         }
 
@@ -116,58 +116,69 @@ namespace PracticeYP.ViewModels
             {
                 var backgroundBrush = Brushes.White;
                 var textBrush = Brushes.Black;
-                var lineBrush = Brushes.LightGray;
 
                 // Заливка фона белым цветом
                 context.FillRectangle(backgroundBrush, new Rect(0, 0, 400, 150));
 
                 // Исправление параметров отображаемого текста капчи
                 var formattedText = new FormattedText(
-                    text,                             // Текст
-                    CultureInfo.CurrentCulture,       //Так и не разобрался для чего это нужно
-                    FlowDirection.LeftToRight,        // Направление текста
-                    Typeface.Default,                 // Шрифт
-                    56,                               // Размер шрифта
-                    Brushes.Black                     // Цвет текста
+                    text,
+                    CultureInfo.CurrentCulture,
+                    FlowDirection.LeftToRight,
+                    Typeface.Default,
+                    56,
+                    Brushes.Black
                 );
 
                 // Определение позиции на изображении, где будет отрисован текст
                 var textPosition = new Point(
-                    (400 - formattedText.Width) / 2,  // Ширина
-                    (150 - formattedText.Height) / 2   // Длина
+                    (400 - formattedText.Width) / 2,
+                    (150 - formattedText.Height) / 2
                 );
-
 
                 context.DrawText(formattedText, textPosition);
 
-
                 var random = new Random();
-                int numberOfLines = random.Next(98, 100);
 
-                for (int i = 0; i < numberOfLines; i++)
+                // Добавление случайных кругов
+                int numberOfCircles = random.Next(20, 30);
+                for (int i = 0; i < numberOfCircles; i++)
                 {
-                    // Случайный выбор цвета полосы, которая будет в капче
-                    var lineColor = new SolidColorBrush(Color.FromArgb(
-                        (byte)random.Next(256),  // Случайный альфа код
-                        (byte)random.Next(256),  // Случайный красный код
-                        (byte)random.Next(256),  // Случайный зеленый код
-                        (byte)random.Next(256)   // Случайный синий код
+                    var circleColor = new SolidColorBrush(Color.FromArgb(
+                        (byte)random.Next(150, 200),  // Случайный альфа код
+                        (byte)random.Next(256),       // Случайный красный код
+                        (byte)random.Next(256),       // Случайный зеленый код
+                        (byte)random.Next(256)        // Случайный синий код
                     ));
 
-                    // Задаётся случайное направление т.е. горизонтальное или вертикальное
-                    bool isHorizontal = random.Next(2) == 0;
+                    double radius = random.Next(10, 30);
+                    double xPosition = random.Next(0, 400 - (int)radius * 2);
+                    double yPosition = random.Next(0, 150 - (int)radius * 2);
 
-                    double lineWidth = isHorizontal ? 400 : random.Next(3, 5);  // Ширина полосы для горизонтальных полос
-                    double lineHeight = isHorizontal ? random.Next(3, 5) : 150;  // Высота полосы для вертикальных полос
+                    context.DrawEllipse(circleColor, null, new Point(xPosition + radius, yPosition + radius), radius, radius);
+                }
 
+                // Добавление случайных линий с разной толщиной
+                int numberOfLines = random.Next(5, 10);
+                for (int i = 0; i < numberOfLines; i++)
+                {
+                    var lineColor = new SolidColorBrush(Color.FromArgb(
+                        (byte)random.Next(100, 150),  // Случайный альфа код
+                        (byte)random.Next(256),       // Случайный красный код
+                        (byte)random.Next(256),       // Случайный зеленый код
+                        (byte)random.Next(256)       // Случайный синий код
+                    ));
 
-                    double xPosition = isHorizontal ? 0 : random.Next(0, 400 - (int)lineWidth);  // Случайное положение для вертикальных полос
-                    double yPosition = isHorizontal ? random.Next(0, 150 - (int)lineHeight) : 0; // Случайное положение для горизонтальных полос
+                    double thickness = random.Next(1, 3);
+                    double startX = random.Next(0, 400);
+                    double startY = random.Next(0, 150);
+                    double endX = random.Next(0, 400);
+                    double endY = random.Next(0, 150);
 
-
-                    context.FillRectangle(lineColor, new Rect(xPosition, yPosition, lineWidth, lineHeight));
+                    context.DrawLine(new Pen(lineColor, thickness), new Point(startX, startY), new Point(endX, endY));
                 }
             }
+
             return renderTarget;
         }
     }
